3fc4fa880faed2b93366551716d6fb5322809e1f
MarceloVichar <marcos.marcelo43@gmail.com>
2024-04-15 11:08:14 -0300

    adicionada tela de login

---
+++ project/elements/auth/LoginForm.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {View} from "react-native";
+import {Button, Icon, Input} from "@rneui/themed";
+import {router} from "expo-router";
+
+export default function LoginForm() {
+    return (
+        <View className="flex-1">
+            <Input placeholder="Digite seu email" label="Email" className="w-full" leftIcon={<Icon name="email"/>}/>
+            <Input placeholder="Digite sua senha" label="Senha" secureTextEntry={true} className="w-full"
+                   leftIcon={<Icon name="password"/>}/>
+            <Button radius={"sm"} type="solid" color="primary" onPress={() => router.push('/operator/events')}>
+                Entrar
+            </Button>
+        </View>
+    );
+}
